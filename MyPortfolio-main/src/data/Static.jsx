f359d6d883828c32d430cefa7033844aae12380b
mitaka3 <justinbituin1207@gmail.com>
2023-11-14 01:35:43 +0800

    initial commit

---
+++ MyPortfolio-main/src/data/Static.jsx
@@ -0,0 +1,138 @@
+import html from "../assets/langues/HTML.png";
+import reactLangue from "../assets/langues/react.png";
+import django from "../assets/langues/django.png";
+import css from "../assets/langues/css.png";
+import github from "../assets/langues/github.png";
+import tailwind from "../assets/langues/tailwind.png";
+import js from "../assets/langues/js.png";
+import bootstrap from "../assets/langues/bootstrap.png";
+import c from "../assets/langues/c.png";
+import py from "../assets/langues/py.png";
+import php from "../assets/langues/php.png";
+import java from "../assets/langues/java.png";
+
+// Array of work objects
+export const works = [
+
+
+
+  {
+    title: "Portfolio Maker",
+    imgSrc:
+      "https://1drv.ms/i/c/da8bdbbd5cc9590f/Eat1BEHo3uZNsGWwYGeUKuMB2Ez5js_SxvxECNQVE2g4oQ?e=7fJ4il",
+    tech: ["HTML", "CSS", "JAVASCRIPT"],
+    workUrl: "https://github.com/mitaka3/Portfolio-bituin-justin-luis",
+    duration: "600",
+  },
+  {
+    title: "Hotel databased",
+    imgSrc:
+      "https://cdn.dribbble.com/users/6870886/screenshots/19057557/cover_dribbble.png",
+    tech: ["HTML", "CSS", "Javascript","PHP","Bootstrap"],
+    workUrl:
+      "https://www.behance.net/gallery/150189805/MAKLTI-Food-Delivery-App",
+    duration: "700",
+  },
+];
+
+
+// Array of experience objects
+export const experiences = [
+  {
+    date: "January 2022 - March 2020",
+    company: "Dashmarie07",
+    position: "Junior developer",
+    description:
+      "I work at a shop in manila and part of my job involves inputting data into the business's excel sheets. i've also edited the shops logo previously and i have knowledge of HTML and css to create awesome website. i created and design a website for the shop, but the project didnt continue because i worked there for only 3 or 4 months.",
+  },
+  {
+    date: "January 2023 - March 2023",
+    company: "unknown company",
+    position: "Internship",
+    description:
+      "i dont have experience but we are doing it on next sem.",
+  },
+
+];
+
+
+// Array of langue objects
+export const langues = [
+  {
+    id: 1,
+    src: html,
+    title: "HTML",
+    style: "shadow-orange-500",
+  },
+  {
+    id: 2,
+    src: css,
+    title: "CSS",
+    style: "shadow-blue-500",
+  },
+  {
+    id: 3,
+    src: js,
+    title: "JavaScript",
+    style: "shadow-yellow-500",
+  },
+  {
+    id: 4,
+    src: reactLangue,
+    title: "React",
+    style: "shadow-sky-500",
+  },
+  {
+    id: 5,
+    src: github,
+    title: "GitHub",
+    style: "shadow-gray-400",
+  },
+  {
+    id: 6,
+    src: tailwind,
+    title: "Tailwind",
+    style: "shadow-teal-600",
+  },
+
+  {
+    id: 8,
+    src: django,
+    title: "Django",
+    style: "shadow-emerald-700",
+  },
+ 
+
+  {
+    id: 11,
+    src: bootstrap,
+    title: "Bootstrap",
+    style: "shadow-violet-500",
+  },
+
+  {
+    id: 13,
+    src: py,
+    title: "Python",
+    style: "shadow-yellow-400",
+  },
+
+  {
+    id: 14,
+    src: php,
+    title: "Php",
+    style: "shadow-violet-400",
+  },
+  {
+    id: 15,
+    src: c,
+    title: "C",
+    style: "shadow-blue-400",
+  },
+  {
+    id: 16,
+    src: java,
+    title: "Java",
+    style: "shadow-orange-500",
+  },
+];
